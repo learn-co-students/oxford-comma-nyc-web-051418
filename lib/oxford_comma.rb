2630e4240a9150623ebd6f6d504cf51748535a6c
Jordan Eckowitz <jordan.eckowitz@gmail.com>
2018-05-03 22:16:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    return array.join(" and ")
+  elsif array.length >= 3
+    second_last_entry = array.length - 2 
+    last_entry = array.length - 1
+    msg = array[0..second_last_entry].join(", ")
+    return "#{msg}, and #{array[last_entry]}"
+  end
 end
