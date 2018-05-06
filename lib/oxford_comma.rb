79a5f8e6730772ee6255c638d0a3e4721b7fff42
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-05 20:15:32 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array << new_last_array_item
+    array.join(", ")
+  end
 end
