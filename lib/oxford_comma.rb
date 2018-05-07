ae21943fe2da3817769753bb89cb4916ba31676b
Tony Ho <ho.tonym@gmail.com>
2018-05-07 09:05:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
+  word = String.new
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last = array.pop
+      for element in array
+        word << "#{element}, "
+      end
+    word << "and #{last}"
+    return word
+  end
 end
