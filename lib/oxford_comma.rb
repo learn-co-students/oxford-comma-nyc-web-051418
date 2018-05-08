228ca75d69e9e49a7794502e934499e5ff499abc
GregS30 <gjschmitt@live.com>
2018-05-08 17:01:01 -0400

    lesson done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  item_count = array.size
+
+  if item_count == 1
+    return array[0]
+  elsif item_count == 2
+    return array.join(" and ")
+  else
+    last_item = array.pop
+    return array.join(", ") + ", and " + last_item
+  end
+
 end
